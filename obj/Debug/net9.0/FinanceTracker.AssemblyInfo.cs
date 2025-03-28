473a09a5080993d757ab55d5eb0e3443a2014b8d
jacobzenner <jacob.zenner@jacks.sdstate.edu>
2025-03-27 20:30:38 -0500

    Update commit

--- obj/Debug/net9.0/FinanceTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/FinanceTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b4482b857629d5d12bbfaa5fc38621aa036a21")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
